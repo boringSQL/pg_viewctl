72ba451975be518b405487c5559c091274805006
Radim Marek <radim@boringsql.com>
2026-03-02 19:30:05 +0100

    fix: missing sql_queries

---
+++ sql_queries/create_test_view.sql
@@ -0,0 +1,2 @@
+CREATE TABLE test_base (id int, old_col text, new_col text);
+CREATE VIEW test_view AS SELECT * FROM test_base
---
+++ sql_queries/insert_test_deprecated_column.sql
@@ -0,0 +1,3 @@
+INSERT INTO pgvc_deprecated_columns
+    (schema_name, view_name, column_name, deprecation_message, removal_date)
+VALUES ('public', 'my_view', 'old_col', 'Use new_col instead', '2026-06-01')
