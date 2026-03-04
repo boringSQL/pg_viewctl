a719a7cc9baadec426c832b38dae87bb6aa3d4e9
Radim Marek <radim@boringsql.com>
2026-03-04 22:41:56 +0100

    chore: validate presence of base table column

---
+++ sql_queries/generate_drop_column_validate.sql
@@ -0,0 +1,11 @@
+SELECT 1
+FROM pg_class c
+JOIN pg_namespace n ON c.relnamespace = n.oid
+JOIN pg_attribute a ON a.attrelid = c.oid
+WHERE
+  n.nspname = $1
+  AND c.relname = $2
+  AND a.attname = $3
+  AND c.relkind = 'r'
+  AND NOT a.attisdropped
+  AND a.attnum > 0
