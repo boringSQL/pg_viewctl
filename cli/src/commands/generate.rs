7fc8d5199876381fedb9483c2287caf16fa6420e
Radim Marek <radim@boringsql.com>
2026-03-06 21:20:36 +0100

    fix: forgot to use generate names only

--- cli/src/commands/generate.rs
+++ cli/src/commands/generate.rs
@@ -8,7 +8,7 @@ pub fn run(client: &mut Client, operation: &Operation) -> Vec<MigrationStep> {
         Operation::DropColumn { target } => {
             let t = crate::parse::parse_three_part(target);
             client.query(
-                "SELECT step, operation, sql FROM pgvc_generate_drop_column($1, $2, $3)",
+                "SELECT step, operation, sql FROM generate_drop_column($1, $2, $3)",
                 &[&t.schema, &t.object, &t.column],
             )
         }
@@ -16,21 +16,21 @@ pub fn run(client: &mut Client, operation: &Operation) -> Vec<MigrationStep> {
             let t = crate::parse::parse_two_part(target);
             let def = read_definition(definition);
             client.query(
-                "SELECT step, operation, sql FROM pgvc_generate_replace_view($1, $2, $3)",
+                "SELECT step, operation, sql FROM generate_replace_view($1, $2, $3)",
                 &[&t.schema, &t.object, &def],
             )
         }
         Operation::AlterType { target, new_type } => {
             let t = crate::parse::parse_three_part(target);
             client.query(
-                "SELECT step, operation, sql FROM pgvc_generate_alter_type($1, $2, $3, $4)",
+                "SELECT step, operation, sql FROM generate_alter_type($1, $2, $3, $4)",
                 &[&t.schema, &t.object, &t.column, new_type],
             )
         }
         Operation::RenameViewColumn { target, new_name } => {
             let t = crate::parse::parse_three_part(target);
             client.query(
-                "SELECT step, operation, sql FROM pgvc_generate_rename_view_column($1, $2, $3, $4)",
+                "SELECT step, operation, sql FROM generate_rename_view_column($1, $2, $3, $4)",
                 &[&t.schema, &t.object, &t.column, new_name],
             )
         }
